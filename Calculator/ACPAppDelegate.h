d894823f675354ddca5610c2094a6ed05cdc1711
A.Parks <thalia1.618@gmail.com>
2013-02-02 10:41:05 -0500

    Initial Commit

---
+++ Calculator/ACPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ACPAppDelegate.h
+//  Calculator
+//
+//  Created by Anna Parks on 2/2/13.
+//  Copyright (c) 2013 Anna Parks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/ACPViewController.h
@@ -0,0 +1,13 @@
+//
+//  ACPViewController.h
+//  Calculator
+//
+//  Created by Anna Parks on 2/2/13.
+//  Copyright (c) 2013 Anna Parks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACPViewController : UIViewController
+
+@end
